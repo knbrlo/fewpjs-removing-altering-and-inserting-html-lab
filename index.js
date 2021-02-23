3d6724d4c7403a4ecac7884d6d073d05faf346e9
Ken Barlow <kenbrlow@gmail.com>
2021-02-23 14:52:28 -0700

    commit tree ad01248d03c387d92e8f7a782df94f97dabf7932

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+// 1 - Remove the main item
+let mainElement = document.getElementById('main');
+mainElement.remove();
+
+// 2 - Add the newHeader
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Ken is the champion!';
